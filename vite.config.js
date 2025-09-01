2d699143f9d0364d8be9fbe9a8cadbb170ef82b7
Purushendra <purushendrakarn@gmail.com>
2025-09-02 03:59:52 +0530

    fix: clean repo after accidental submodule

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/red/",
+  base: '/Redcube/', // 👈 replace with your repo name
   plugins: [react()],
 });
