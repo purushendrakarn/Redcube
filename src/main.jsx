9a8e76e1f38d4d0cc8159a148ff4433f550cb6ef
Purushendra <purushendrakarn@gmail.com>
2025-09-04 21:37:46 +0530

    Fix vite config for custom domain

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-
-
-const basename = import.meta.env.MODE === "production" ? "/Redcube" : "/";
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.MODE === "production" ? "/Redcube" : "/"}>
+    <BrowserRouter> {/* ✅ no basename, root domain only */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
